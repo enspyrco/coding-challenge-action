9b3cf80dde2a526e8bec89a1bcb2dca9b7c2b7e2
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-21 16:53:14 +1100

    Update dart app to print / dir

--- print_files/bin/print_files.dart
+++ print_files/bin/print_files.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main(List<String> arguments) async {
-  final dir = Directory('/app/bin/print_files');
+  final dir = Directory('/');
   final List<FileSystemEntity> entities = await dir.list().toList();
   entities.forEach(print);
 }
