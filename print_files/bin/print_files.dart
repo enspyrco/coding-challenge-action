d7311e248261e46abbef78b186f2460a6bec7da4
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-21 19:22:21 +1100

    Use base sha to get original files

--- print_files/bin/print_files.dart
+++ print_files/bin/print_files.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 void main(List<String> arguments) async {
   var workspacePath = Platform.environment['GITHUB_WORKSPACE']!;
 
-  print('Printing contents of : $workspacePath/prhead');
-  final dir = Directory('$workspacePath/prhead');
+  print('Printing contents of : $workspacePath/head');
+  final dir = Directory('$workspacePath/head');
   final List<FileSystemEntity> entities = await dir.list().toList();
   entities.forEach(print);
 
-  print('Printing contents of : $workspacePath/main');
-  final dir2 = Directory('$workspacePath/main');
+  print('Printing contents of : $workspacePath/base');
+  final dir2 = Directory('$workspacePath/base');
   final List<FileSystemEntity> entities2 = await dir2.list().toList();
   entities2.forEach(print);
 }
