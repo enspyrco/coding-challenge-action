cd3bf8ed6f7ed6f083d8c173756495a9e741d1a3
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-21 16:44:01 +1100

    Change dir to print in action

--- print_files/bin/print_files.dart
+++ print_files/bin/print_files.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main(List<String> arguments) async {
-  final dir = Directory('.');
+  final dir = Directory('/app/bin/print_files');
   final List<FileSystemEntity> entities = await dir.list().toList();
   entities.forEach(print);
 }
