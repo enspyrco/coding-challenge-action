390d642638139842c02f836dbd6564371474aaf4
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-21 17:04:42 +1100

    Add logging

--- print_files/bin/print_files.dart
+++ print_files/bin/print_files.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main(List<String> arguments) async {
   var workspacePath = Platform.environment['GITHUB_WORKSPACE']!;
-
+  print('Printing contents of : $workspacePath');
   final dir = Directory(workspacePath);
   final List<FileSystemEntity> entities = await dir.list().toList();
   entities.forEach(print);
