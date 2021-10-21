1368a3f75d6bd219ce80db31e106eda31f7fcbeb
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-21 19:00:02 +1100

    Print dir for pr head and merge commit

--- print_files/bin/print_files.dart
+++ print_files/bin/print_files.dart
@@ -2,8 +2,14 @@ import 'dart:io';
 
 void main(List<String> arguments) async {
   var workspacePath = Platform.environment['GITHUB_WORKSPACE']!;
-  print('Printing contents of : $workspacePath');
-  final dir = Directory(workspacePath);
+
+  print('Printing contents of : $workspacePath/prhead');
+  final dir = Directory('$workspacePath/prhead');
   final List<FileSystemEntity> entities = await dir.list().toList();
   entities.forEach(print);
+
+  print('Printing contents of : $workspacePath/main');
+  final dir2 = Directory('$workspacePath/main');
+  final List<FileSystemEntity> entities2 = await dir2.list().toList();
+  entities2.forEach(print);
 }
