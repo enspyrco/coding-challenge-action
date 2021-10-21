006cdde47a18de18189bd1201594629bfda08ca9
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-21 17:00:31 +1100

    Change to printing GITHUB_WORKSPACE dir

--- print_files/bin/print_files.dart
+++ print_files/bin/print_files.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 void main(List<String> arguments) async {
-  final dir = Directory('/');
+  var workspacePath = Platform.environment['GITHUB_WORKSPACE']!;
+
+  final dir = Directory(workspacePath);
   final List<FileSystemEntity> entities = await dir.list().toList();
   entities.forEach(print);
 }
