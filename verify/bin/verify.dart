39ecf56399c3b01d179775eee84a07d7e034c6b5
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-23 12:01:52 +1100

    Add a launch config and debug print

--- verify/bin/verify.dart
+++ verify/bin/verify.dart
@@ -1,1 +1,20 @@
-void main(List<String> arguments) async {}
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) async {
+  var token = 'lajsdfljsf';
+  final queryParameters = {
+    'userId': 'nickmeinhold',
+    'repoName': 'devtools_adventure',
+    'challengeNumber': '2',
+    'prNumber': '33',
+  };
+  final uri = Uri.http('localhost:8080', '', queryParameters);
+  final response = await http.get(uri, headers: {
+    HttpHeaders.authorizationHeader: 'Token $token',
+    HttpHeaders.contentTypeHeader: 'application/json',
+  });
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+}
